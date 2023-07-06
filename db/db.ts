a8e4d035e0f863fdb525af7246fed7012322a999
cyanChill <anthonyliang9@gmail.com>
2023-07-06 17:30:32 -0400

    init app dependencies - Uses Next.js, TypeScript, Tailwind, Drizzle, PlanetScale, Zod

---
+++ db/db.ts
@@ -0,0 +1,11 @@
+import { drizzle } from "drizzle-orm/planetscale-serverless";
+import { connect } from "@planetscale/database";
+
+// Create the connection
+const connection = connect({
+  host: process.env["DATABASE_HOST"],
+  username: process.env["DATABASE_USERNAME"],
+  password: process.env["DATABASE_PASSWORD"],
+});
+
+export const db = drizzle(connection);
