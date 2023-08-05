91f104491d3ba812a81694aad7f19c79c2ba927f
cyanChill <anthonyliang9@gmail.com>
2023-08-05 12:48:31 -0400

    Fixed bug with "Indexed Search" feat.
    - Bug with filter modal closing prematurely
    - Updated tags to be as intended

--- app/(main)/(search)/indexed/_components/filter-buttons.tsx
+++ app/(main)/(search)/indexed/_components/filter-buttons.tsx
@@ -4,7 +4,6 @@ import { Transition } from "@headlessui/react";
 import { FaCogs } from "react-icons/fa";
 import { FaXmark } from "react-icons/fa6";
 
-import { useExitOutRef } from "@/hooks/useExitOutRef";
 import { cn } from "@/lib/utils";
 import type { Option } from "@/components/form/utils";
 import SearchForm from "./search-form";
@@ -16,7 +15,6 @@ type Props = {
 
 export default function FilterButtons({ labels, languages }: Props) {
   const [modalActive, setModalActive] = useState(false);
-  const [ref] = useExitOutRef<HTMLDivElement>(() => setModalActive(false));
 
   /*
     TODO: Eventually add the "Sort" button
@@ -25,45 +23,45 @@ export default function FilterButtons({ labels, languages }: Props) {
 
   return (
     <>
-      <div ref={ref} className="text-lg">
+      <section className="text-lg">
         <button
           onClick={() => setModalActive(true)}
           className="btn just-black flex items-center gap-2 bg-white py-0"
         >
           <FaCogs /> Filters
         </button>
+      </section>
 
-        {/* Filter Menu Modal */}
-        <Transition
-          as="section"
-          show={modalActive}
-          enter="transition ease-out duration-200"
-          enterFrom="transform opacity-0 -translate-x-1/4"
-          enterTo="transform opacity-100 translate-x-0"
-          leave="transition ease-in duration-150"
-          leaveFrom="transform opacity-100 translate-x-0"
-          leaveTo="transform opacity-0 -translate-x-1/4"
-          className="fixed left-0 top-0 z-modal h-screen w-full border-r-3 border-black bg-white p-4 md:max-w-xl"
-        >
-          <header className="mb-8 flex items-center gap-4 text-2xl font-medium md:text-4xl">
-            <FaCogs />
-            <h2>Filters</h2>
-            <button
-              onClick={() => setModalActive(false)}
-              className="ml-auto transition duration-300 hover:text-black/75"
-            >
-              <FaXmark />
-            </button>
-          </header>
+      {/* Filter Menu Modal */}
+      <Transition
+        as="section"
+        show={modalActive}
+        enter="transition ease-out duration-200"
+        enterFrom="transform opacity-0 -translate-x-1/4"
+        enterTo="transform opacity-100 translate-x-0"
+        leave="transition ease-in duration-150"
+        leaveFrom="transform opacity-100 translate-x-0"
+        leaveTo="transform opacity-0 -translate-x-1/4"
+        className="fixed left-0 top-0 z-modal h-screen w-full overflow-y-auto border-r-3 border-black bg-white p-4 md:max-w-xl"
+      >
+        <header className="mb-8 flex items-center gap-4 text-2xl font-medium md:text-4xl">
+          <FaCogs />
+          <h2>Filters</h2>
+          <button
+            onClick={() => setModalActive(false)}
+            className="ml-auto transition duration-300 hover:text-black/75"
+          >
+            <FaXmark />
+          </button>
+        </header>
 
-          <SearchForm
-            labels={labels}
-            languages={languages}
-            values={{}}
-            isSubmitting={false}
-          />
-        </Transition>
-      </div>
+        <SearchForm
+          labels={labels}
+          languages={languages}
+          values={{}}
+          isSubmitting={false}
+        />
+      </Transition>
 
       {/* Backdrop Blur */}
       <Transition
@@ -76,6 +74,7 @@ export default function FilterButtons({ labels, languages }: Props) {
         leaveFrom="transform opacity-100"
         leaveTo="transform opacity-0"
         className="fixed left-0 top-0 z-[125] h-screen w-screen bg-black/50 backdrop-blur-sm"
+        onClick={() => setModalActive(false)}
       />
     </>
   );
