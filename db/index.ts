5afbcbc789b14d0c395119ab43632170547fd38a
cyanChill <anthonyliang9@gmail.com>
2024-02-26 14:14:32 -0500

    chore(deps): Bumped dependencies
    - Updated database connection initialization

--- db/index.ts
+++ db/index.ts
@@ -1,11 +1,11 @@
 import { drizzle } from "drizzle-orm/planetscale-serverless";
-import { connect } from "@planetscale/database";
+import { Client } from "@planetscale/database";
 
 import schema from "./schema";
 
 // Create the connection
-const connection = connect({
-  url: process.env["DATABASE_URL"],
+const connection = new Client({
+  url: process.env.DATABASE_URL,
 });
 
 export const db = drizzle(connection, { schema });
